c6d8b4077d0ea6ec5aac922e7f99e17ed35a136b
seancbs <sean.y@centurybizsolutions.com>
2021-01-03 20:50:53 -0800

    initial

---
+++ ReactNetCoreChat.API/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using ReactNetCoreChat.API.Hubs;
+using ReactNetCoreChat.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCoreChat.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub, IChatClient> _chatHub;
+        public ChatController(IHubContext<ChatHub, IChatClient> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        public async Task Post(ChatMessage message)
+        {
+            await _chatHub.Clients.All.ReceiveMessage(message);
+        }
+    }
+}
---
+++ ReactNetCoreChat.API/Hubs/IChatClient.cs
@@ -0,0 +1,13 @@
+﻿using ReactNetCoreChat.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactNetCoreChat.API.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+    }
+}
